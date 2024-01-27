d762f192bb501f843ca146d3ba74e373f971b66d
DianaPavlova <diana50888@gmail.com>
2024-01-27 08:54:27 +0200

    Добавляет мелкие адаптивные сетки кроме слайдера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
-/* в этот файл добавляет скрипты*/
+const navPage = document.querySelector('.nav-page');
+const navToggle = document.querySelector('.nav-page__toggle');
+
+navPage.classList.remove('nav-page--nojs');
+navPage.classList.add('nav-page--closed');
+navPage.classList.remove('nav-page--opened');
+
+navToggle.addEventListener('click', () => {
+  navPage.classList.toggle('nav-page--closed');
+  navPage.classList.toggle('nav-page--opened');
+});
