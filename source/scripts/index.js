78871d8f7f704978310d1f7a299f5da36bc8f645
DianaPavlova <diana50888@gmail.com>
2024-01-29 08:24:03 +0200

    Дорабатывает проект для защиты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,10 @@
 const navPage = document.querySelector('.nav-page');
 const navToggle = document.querySelector('.nav-page__toggle');
+const contactsMap = document.querySelector('.contacts__map');
+const pageBody = document.querySelector('.page__body');
 
+pageBody.classList.remove('page__body--nojs');
+//contactsMap.classList.remove('contacts__map--nojs');
 navPage.classList.remove('nav-page--nojs');
 navPage.classList.add('nav-page--closed');
 navPage.classList.remove('nav-page--opened');
